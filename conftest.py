0d8ae661297376c283e16560fecd4b5ced095c5c
FILA <fila090580@gmail.com>
2024-05-09 15:26:10 +0300

    add files

---
+++ conftest.py
@@ -0,0 +1,43 @@
+
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+device_name = "192.168.2.31"
+# device_name = "dbe407da"
+
+
+def pytest_addoption(parser):
+    """Опции командной строки.
+    В командную строку передается параметр вида '--language="es"'
+    По умолчанию передается параметр, включающий английский интерфейс в браузере
+    """
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: ---")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def driver(request):
+    """selenium fixture"""
+    print()
+    print("__USE_SELENIUM_FIXTURE__")
+    print("\nstart driver for test..")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
+
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])  # Убираем лишние ошибки из консоли
+
+    user_language = request.config.getoption("language")
+    chrome_options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+
+    request.cls.driver = driver
+    yield driver
+    print("\nquit driver..")
+    driver.quit()
