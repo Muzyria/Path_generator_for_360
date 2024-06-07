45936ef48f11cae824abb1cd31a35c2b656d8bf4
FILA <fila090580@gmail.com>
2024-06-07 18:30:19 +0300

    add files

--- pages/device_pages/settings_date_time_page.py
+++ pages/device_pages/settings_date_time_page.py
@@ -3,12 +3,41 @@ from appium.webdriver.common.appiumby import AppiumBy
 
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class SettingsDateTime(AppiumBasePage):
 
-    def qwerety(self):
-        print("QQQQQQQ")
-        # el = self.appium_driver.find_element(AppiumBy.ID, value='com.l1inc.yamatrack3d:id/buttonMenu')
-        el = self.wait.until(EC.element_to_be_clickable(('id', 'com.l1inc.yamatrack3d:id/buttonMenu')))
-        el.click()
+    SET_POWER_TIME = ("xpath", "(//android.widget.LinearLayout)[17]")
 
+    CLOCK_HOURS = ("id", "android:id/hours")
+    CLOCK_MINUTES = ("id", "android:id/minutes")
 
+    AM_LABEL = ("id", "android:id/am_label")
+    PM_LABEL = ("id", "android:id/pm_label")
+
+    @staticmethod
+    def get_hours_locator(hours_value):
+        return ("xpath", f'//android.widget.RadialTimePickerView.RadialPickerTouchHelper[@content-desc="{hours_value}"]')  # from 1 to 12
+
+    @staticmethod
+    def get_minutes_locator(minutes_value):
+        return ("xpath", f'//android.widget.RadialTimePickerView.RadialPickerTouchHelper[@content-desc="{minutes_value}"]')  # from 0 to 55 by step 5
+
+    BUTTON_OK = ("id", "android:id/button1")
+
+    def click_set_power_time(self):
+        self.wait.until(EC.element_to_be_clickable(self.SET_POWER_TIME)).click()
+
+    def click_pm_label(self):
+        self.wait.until(EC.element_to_be_clickable(self.PM_LABEL)).click()
+
+    def click_am_label(self):
+        self.wait.until(EC.element_to_be_clickable(self.AM_LABEL)).click()
+
+    def set_hours(self, hours_value=10):
+        self.wait.until(EC.element_to_be_clickable(self.get_hours_locator(hours_value))).click()
+
+    def set_minutes(self, minutes_value=0):
+        self.wait.until(EC.element_to_be_clickable(self.get_minutes_locator(minutes_value))).click()
+
+    def button_ok_click(self):
+        self.wait.until(EC.element_to_be_clickable(self.BUTTON_OK)).click()
--- tests/test_power_scheduler.py
+++ tests/test_power_scheduler.py
@@ -4,13 +4,13 @@ import time
 import pytest
 from base.adb_commands import AdbCommands
 
-from appium.webdriver.common.appiumby import AppiumBy
-
 from pages.device_pages.settings_date_time_page import SettingsDateTime
 
 
 class TestPowerScheduler:
     IP = "192.168.0.101"
+
+    settings_date_time: SettingsDateTime
 
     @pytest.fixture(autouse=True)
     def adb_commands(self, request):
@@ -19,16 +19,23 @@ class TestPowerScheduler:
         self.adb_command = AdbCommands(self.IP)
         self.adb_command.device_connect()
 
-    def test_first(self, appium_driver):
+    @pytest.fixture(autouse=True)
+    def setup(self, request, appium_driver):
+        request.cls.settings_date_time = SettingsDateTime(appium_driver)
+
+    def test_first(self):
         """
-        2)Set Power Scheduler at a 9:55 PM, Confirme randomizer's time on Power Scheduler - note it
+        2)Set Power Scheduler at a 9:55 PM, Confirm randomizer's time on Power Scheduler - note it
         4) Wait randomizer's time and confirm device restart.
         """
-        # el = appium_driver.find_element(AppiumBy.ID, value='com.l1inc.yamatrack3d:id/buttonMenu')
-        # el.click()
-        print("TEST__")
-        test = SettingsDateTime(appium_driver)
-        test.qwerety()
+
+        print("TEST __ START")
+        self.adb_command.open_date_settings()
+
+        self.settings_date_time.click_set_power_time()
+
+        self.settings_date_time.click_pm_label()
+        self.settings_date_time.set_hours(6)
+        self.settings_date_time.set_minutes(20)
+        self.settings_date_time.button_ok_click()
         print("TEST ____ FINISH")
-
-
